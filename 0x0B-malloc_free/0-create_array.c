15318e876ca98a9ad7604aa20f5d3d868faa4dea
joviah1 <joviahanyango12@gmail.com>
2023-07-10 04:52:33 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,23 +6,27 @@
  * @size: The size of array
  * @c: The char
  *
- * Return: The array filled
+ * Return: The array filed
  */
-char *create_array(unsigned int size, char c);
+
+char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *j;
+    unsigned int i;
+    char *j;
 
-	if (size == 0)
-		return (NULL);
+    if (size == 0)
+        return (NULL);
 
-	j = malloc(size * sizeof(char));
+    j = malloc(size * sizeof(char));
 
-	if (j == 0)
-		return (NULL);
-	for (i = 0; i < size; i++)
-	{
-		j[i] = c;
-	}
-	return (j);
+    if (j == NULL)
+        return (NULL);
+
+    for (i = 0; i < size; i++)
+    {
+        j[i] = c;
+    }
+
+    return (j);
 }
+
